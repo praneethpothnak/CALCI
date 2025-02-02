8dd5a76cb1966fc3aead20a14058d8a023b97f9a
PraneethPothank <112543221+praneethpothnak@users.noreply.github.com>
2025-02-03 02:23:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UltimateCalculator from "./UltimateCalculator"; // Import your UltimateCalculator component
+
+function App() {
+  return (
+    <div className="App">
+      <UltimateCalculator /> {/* Render the UltimateCalculator */}
+    </div>
+  );
+}
+
+export default App;
